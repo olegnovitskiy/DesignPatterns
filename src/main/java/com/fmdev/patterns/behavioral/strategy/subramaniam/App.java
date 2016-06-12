79e9d06a11159d19aa2d69c65129bed07a5f3b84
Oleg Novitskiy <oleg.novitskiy.dev@gmail.com>
2016-06-12 23:40:17 +0300

    Add lambda lightweight Strategy Step3

--- src/main/java/com/fmdev/patterns/behavioral/strategy/subramaniam/App.java
+++ src/main/java/com/fmdev/patterns/behavioral/strategy/subramaniam/App.java
@@ -9,8 +9,8 @@ public class App {
         List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
 
         System.out.println(totalValues(numbers, e -> true));
-        System.out.println(totalValues(numbers, e -> e % 2 == 0));
-        System.out.println(totalValues(numbers, e -> e % 2 != 0));
+        System.out.println(totalValues(numbers, Util::isEven));
+        System.out.println(totalValues(numbers, Util::isOdd));
     }
 
     private static int totalValues(List<Integer> values, Predicate<Integer> selector) {
@@ -27,5 +27,15 @@ public class App {
 //        return values.stream()
 //                     .filter(selector)
 //                     .reduce(0, Integer::sum);
+    }
+}
+
+class Util {
+    static boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+
+    static boolean isOdd(int number) {
+        return number % 2 != 0;
     }
 }
