bcbfd4d33729689799960f883af33dd1b02ff699
Oleg Novitskiy <oleg.novitskiy.dev@gmail.com>
2016-06-12 23:32:40 +0300

    Add lambda lightweight Strategy Step1

--- src/main/java/com/fmdev/patterns/behavioral/strategy/subramaniam/App.java
+++ src/main/java/com/fmdev/patterns/behavioral/strategy/subramaniam/App.java
@@ -2,44 +2,24 @@ package com.fmdev.patterns.behavioral.strategy.subramaniam;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class App {
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
 
-        // total all values
-        System.out.println(totalValues(numbers));
-        System.out.println(totalEvenValues(numbers));
-        System.out.println(totalOddValues(numbers));
+        System.out.println(totalValues(numbers, e -> true));
+        System.out.println(totalValues(numbers, e -> e % 2 == 0));
+        System.out.println(totalValues(numbers, e -> e % 2 != 0));
     }
 
-    private static int totalOddValues(List<Integer> values) {
-        int result = 0;
-
-        for (int e : values) {
-            if (e % 2 != 0) {
-                result += e;
-            }
-        }
-        return result;
-    }
-
-    private static int totalEvenValues(List<Integer> values) {
-        int result = 0;
-
-        for (int e : values) {
-            if (e % 2 == 0) {
-                result += e;
-            }
-        }
-        return result;
-    }
-
-    private static int totalValues(List<Integer> values) {
+    private static int totalValues(List<Integer> values, Predicate<Integer> selector) {
         int result = 0;
 
         for (int e: values) {
-            result += e;
+            if(selector.test(e)) {
+                result += e;
+            }
         }
 
         return result;
