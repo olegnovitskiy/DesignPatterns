7d9d8e82afe3a731d06bdcf552e2b39936759e2f
Oleg Novitskiy <oleg.novitskiy.dev@gmail.com>
2016-06-13 00:22:48 +0300

    Add lambda Decorator Camera Step 2

--- src/main/java/com/fmdev/patterns/structural/decorator/subramaniam/App.java
+++ src/main/java/com/fmdev/patterns/structural/decorator/subramaniam/App.java
@@ -1,19 +1,40 @@
 package com.fmdev.patterns.structural.decorator.subramaniam;
+
 import java.util.function.*;
 import java.awt.*;
+import java.util.stream.Stream;
 
+@SuppressWarnings("unchecked")
 class Camera {
+    private Function<Color, Color> filter;
+
+    Camera(Function<Color, Color>... filters) {
+        setFilters(filters);
+    }
+
+    private void setFilters(Function<Color, Color>... filters) {
+        filter = Stream.of(filters)
+                .reduce(
+                        Function.identity(),
+                        Function::andThen
+                );
+    }
+
     Color snap(Color input) {
-        return input;
+        return filter.apply(input);
     }
 }
 
+@SuppressWarnings("unchecked")
 public class App {
     private static void printSnap(Camera camera) {
-        System.out.println(camera.snap(new Color(125,125,125)));
+        System.out.println(camera.snap(new Color(125, 125, 125)));
     }
 
     public static void main(String[] args) {
         printSnap(new Camera());
+        printSnap(new Camera(Color::brighter));
+        printSnap(new Camera(Color::darker));
+        printSnap(new Camera(Color::brighter, Color::darker));
     }
 }
